3fb394cb3e899925cef504c4e78d01acca930ac0
Hamilton do Vale Oliveira <hamilton.valnet@gmail.com>
2025-08-18 13:59:36 -0300

    Inicio

---
+++ GuardaSolution/src/Domain/Arquivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Arquivo
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string NomeOriginal { get; set; }
+        public string CaminhoArmazenado { get; set; }
+        public string Extensao { get; set; }
+        public long TamanhoBytes { get; set; }
+        public DateTime DataUpload { get; set; } = DateTime.UtcNow;
+
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public string TipoDocumento { get; set; }
+        public string ChaveAcesso { get; set; }
+        public string CnpjEmitente { get; set; }
+        public string CnpjDestinatario { get; set; }
+        public DateTime? DataEmissao { get; set; }
+
+        public ICollection<LogProcessamento> Logs { get; set; } = new List<LogProcessamento>();
+    }
+}
